6a87f0fcac0031ee1a54b94dae399be9e01a179c
Lucidity <2993514952@qq.com>
2022-09-01 18:18:31 +0800

    add Test.java

---
+++ Sort/src/cn/javaguide/BubbleSort/Test.java
@@ -0,0 +1,14 @@
+package cn.javaguide.BubbleSort;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 6, 2, 49, 4, 8, 1, 45, 8, 2, 6, 3};
+        System.out.println(Arrays.toString(bubbleSort.bubble_Sort(a)));
+
+
+
+
+    }
+}
--- Sort/src/cn/javaguide/BubbleSort/bubbleSort.java
+++ Sort/src/cn/javaguide/BubbleSort/bubbleSort.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class bubbleSort {
     public static void main(String[] args) {
         int[] a = new int[]{1, 6, 2, 49, 4, 8, 1, 45, 8, 2, 6, 3};
-        System.out.println(Arrays.toString(bubbleSort(a)));
+        System.out.println(Arrays.toString(bubble_Sort(a)));
     }
     /**
      * 冒泡排序
-     * @param arr
-     * @return arr
+     * @param arr 要排序的数组
+     * @return arr  被排序好的数组
      */
-    public static int[] bubbleSort(int[] arr) {
+    public static int[] bubble_Sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             // Set a flag, if true, that means the loop has not been swapped,
             // that is, the sequence has been ordered, the sorting has been completed.
